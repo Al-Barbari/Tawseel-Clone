d4739828350b7ce1dccf635425d623a83df4c9e7
Abdulelah Al-Barbari <AbdulelahAlbarbari1@gmail.com>
2024-11-11 21:21:26 +0300

    Initail Commit

---
+++ Tawseel.WebAPI/Program.cs
@@ -0,0 +1,33 @@
+using Serilog;
+using Tawseel.Application;
+using Tawseel.Infrastructure;
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
